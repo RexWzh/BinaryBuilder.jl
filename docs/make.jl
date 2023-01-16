df8a2ab9948d8b133c4aeea95688d7269876cda5
rex <1073853456@qq.com>
2023-01-17 01:50:29 +0800

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,24 @@ makedocs(
     modules = [BinaryBuilder],
     sitename = "BinaryBuilder.jl",
     pages = [
-        "Home" => "index.md",
-        "Building Packages" => "building.md",
-        "Build Tips" => "build_tips.md",
-        "JLL packages" => "jll.md",
-        "FAQ" => "FAQ.md",
-        "Build Troubleshooting" => "troubleshooting.md",
-        "Internals" => [
-            "RootFS" => "rootfs.md",
-            "Environment Variables" => "environment_variables.md",
-            "Tricksy Gotchas" => "tricksy_gotchas.md",
-            "Reference" => "reference.md",
+        "主页" => "index.md",
+        "构建包" => "building.md",
+        "包的构建技巧" => "build_tips.md",
+        "JLL 包" => "jll.md",
+        "常见问题" => "FAQ.md",
+        "构建故障排除" => "troubleshooting.md",
+        "内部" => [
+            "根文件系统" => "rootfs.md",
+            "环境变量" => "environment_variables.md",
+            "棘手的问题" => "tricksy_gotchas.md",
+            "引用" => "reference.md",
         ],
     ],
-    strict = true,
+    # strict = true,
 )
 
 deploydocs(
     repo = "github.com/RexWzh/BinaryBuilder.jl.git",
     # push_preview = true,
+    versions = ["Chinese" => "v0.0.2", "Mixed" => "v0.0.1"]
 )
